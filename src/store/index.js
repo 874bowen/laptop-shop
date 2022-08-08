aac92a86933543830ec5514842e13d148dcda92e
Ivan Bowen <bowenivan16@gmail.com>
2022-08-08 07:50:18 +0300

    add reducer functions to manage the state and update data

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,27 @@ import { configureStore } from '@reduxjs/toolkit';
 
 
 const reducerFn = (state={counter: 0}, action) => {
+    // limitations: 
+    // should be a synchronous function
+    // we shouldn't mutate the state rather just have a copy of original state
+    switch (action.type) {
+        case "INC":
+            return {counter: state.counter+1};
+            break;
+        case "DEC":
+            if (state.counter >0){
+                return {counter: state.counter-1};
+            }
+            else {
+                return {counter: state.counter};
+            }
+            break;
+        case "ADD":
+            return {counter: state.counter+ action.payload};
+            break;
+        default:
+            break;
+    }
     return state;
 }
 
