161c62b6fc5a2e6702e10ed06c492f8b742db88e
Ivan Bowen <bowenivan16@gmail.com>
2022-08-08 21:15:04 +0300

    use MAinComponent as the container component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Button } from 'reactstrap'
+import { Button } from 'reactstrap';
+import {actions} from './store/index';
+import Main from './components/MainComponent';
 
 function App() {
-  // pass callback function
-  const counter = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
-  const increment = () => {dispatch({type: 'INC'});}
-  const decrement = () => {dispatch({type: 'DEC'});}
-  const add = () => {dispatch({type: 'ADD', payload: 10});}
-  return (
-    <div className='container'>
-      <div className='row mt-5'>
-        <div>
-          <h1>Counter App</h1>
-          <h2> This is {counter}</h2>
-          <Button onClick={increment} className='outline success'>Increment</Button>
-          <Button outline onClick={decrement} className='outline success'>Decrement</Button>
-          <Button onClick={add} className='outline success'>Add 10</Button>
-        </div>
-      </div>
-    </div>
+  return(
+    <Main />
   );
 }
-
 export default App;
