5b314cd6569cae4d084a39551c967ca5b2e2e09f
Ivan Bowen <bowenivan16@gmail.com>
2022-08-12 07:51:30 +0300

    add items to cart by dispatching the function in ProductComponent, useSelector to grab the state.

--- src/components/CartComponent.js
+++ src/components/CartComponent.js
@@ -1,8 +1,9 @@
 import { Button } from 'reactstrap';
-import React, { Component } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Cart =  () => {
-    const quantity = 5;
+    const quantity = useSelector((state) => state.cart.totalQuantity);
     return(
         <div><Button outline color=''><h5>Cart: {quantity} items</h5></Button></div>
     );
--- src/components/PraoductComponent.js
+++ src/components/PraoductComponent.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Card, Button } from 'reactstrap';
+import { cartActions } from "../store/CartSlice";
 
 const Product = ({ id, name, imgURL, price }) => {
+    const dispatch = useDispatch();
+    const increment = () => {
+        dispatch(cartActions.increment());
+    }
     return(
         <Card className="">
             <img src={imgURL} alt={name} />
@@ -11,7 +17,7 @@ const Product = ({ id, name, imgURL, price }) => {
             <h2 className="text-danger">$ {price}</h2>
             </div>
             </div>
-            <p className="text-center"><Button outline color="primary" width="50">Add to Cart</Button></p>
+            <p className="text-center"><Button outline color="primary" width="50" onClick={increment}>Add to Cart</Button></p>
         </Card>
     );
 }
