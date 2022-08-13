275c55cdf2e33bcae0ceffafa9d172c1c2abcec8
Ivan Bowen <bowenivan16@gmail.com>
2022-08-13 16:08:00 +0300

    add the add to cart reducer function

--- src/store/CartSlice.js
+++ src/store/CartSlice.js
@@ -8,9 +8,41 @@ const CartSlice = createSlice({
         showCart: false,
     },
     reducers: {
-        increment (state) { state.totalQuantity++; },
-        decrement (state) { state.items--; },
-        addItems (state, item){ state.itemsList.push(item); }
+        addToCart (state, action) {
+            const newItem = action.payload;
+            // check for existance of same product
+            const existingItem = state.itemsList.find((item) => item.id === newItem.id);
+
+            if ( existingItem ) {
+                existingItem.quantity++;
+                existingItem.totalPrice += newItem.price;
+            } else{
+                state.itemsList.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.name,
+                });
+            }
+            state.totalQuantity++; 
+        },
+        removeItem (state, action) {
+            // remove an item from 2 or more
+            const id = action.payload;
+            const prod = state.itemsList.find((item) => item.id === id);
+            
+            if (prod){
+                if(prod.quantity > 1){
+                    prod.totalQuantity--;
+                    prod.totalPrice -= prod.price;
+                }
+            }
+            else {
+                const index = state.itemsList.indexOf(prod);
+                state.itemsList.splice(index, 1);
+            }
+        },
     }
 });
 
