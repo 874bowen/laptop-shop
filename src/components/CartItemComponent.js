e392a30b9e78dd8c7515100965b9af00e552df41
Ivan Bowen <bowenivan16@gmail.com>
2022-08-13 16:10:53 +0300

    add dispatcher and get the state

--- src/components/CartItemComponent.js
+++ src/components/CartItemComponent.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { useDispatch } from "react-redux/es/exports";
-import { Button, Article } from "reactstrap";
-const CartItem = ({name, quantity, total, price}) => {
-    const removeHandler = () => {};
-    const addHandler = () => {};
+import { Button } from "reactstrap";
+import { cartActions } from "../store/CartSlice";
+const CartItem = ({id, name, quantity, total, price}) => {
+    const dispatch = useDispatch();
+    const addHandler = () => {
+        dispatch(cartActions.addToCart({id, name, price}));
+    }
+    const removeHandler = () => {
+        dispatch(cartActions.removeItem({id}));
+    };
     return(
         <div className="container">
-            <h2>{name}</h2>
-            <p>${price}</p>
-            <p>x{quantity}</p>
-            <Article>Total ${total}</Article>
-            <Button onClick={removeHandler}>-</Button>
-            <Button onClick={addHandler}>+</Button>
+            <div className="row">
+                <div className="col-4"><p><h2>{name}</h2></p></div>
+                <div className="col-2"><p>${price}</p></div>
+                <div className="col-2"><p>x{quantity}</p></div>
+                <div className="col-2"><article>Total ${total}</article></div>
+                <div className="col-1">
+                    <Button onClick={removeHandler}>-</Button></div>
+                    <div className="col-1"><Button onClick={addHandler}>+</Button>
+                </div>
+            </div>
         </div>
     );    
 };
--- src/components/CartItemsComponent.js
+++ src/components/CartItemsComponent.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import CartItem from "./CartItemComponent";
 
 const CartItems = () => {
+    const cartItems = useSelector(state => state.cart.itemsList);
     return(
         <div className="container">
             <h2>Your Cart</h2>
             <ul>
-                <li>
-                    <CartItem id={1} price={2500} name={"MacBook"} />
-                </li>
+                {cartItems.map(item => (
+                    <li key={item.id}>
+                        <CartItem 
+                            id={item.id}
+                            quantity={item.quantity} 
+                            price={item.price}  
+                            name={item.name}
+                            total={item.totalPrice} />
+                    </li>
+                ))}
+                
             </ul>
         </div>
     );
