16a0d2438bbac6a266028b2b8a1480cb03b8f7c5
Ivan Bowen <bowenivan16@gmail.com>
2022-08-15 21:20:15 +0300

    add logout dispatch function

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { useDispatch } from 'react-redux';
+import { Button } from 'reactstrap';
+import { authActions } from '../store/AuthSlice';
+import { cartActions } from '../store/CartSlice';
 import Cart from './CartComponent';
 const Header =  () => {
+    const dispatch = useDispatch();
+    const logout = () => {
+        dispatch(authActions.logout());
+    }
     return(
         <div className='container'>
             <div className='d-flex justify-content-between'>
                 <div><h2>Redux Shopping APP</h2></div>
                 <Cart />
+                <Button onClick={logout}>Logout</Button>
             </div>
             
         </div>
