6d0702a15f12327e68e43c3274538275b9ae25cb
Ivan Bowen <bowenivan16@gmail.com>
2022-08-15 21:21:04 +0300

    add getTotal functionality

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { useSelector } from 'react-redux';
+import { Button } from 'reactstrap';
 import CartItems from './CartItemsComponent';
 
 
@@ -8,11 +9,20 @@ import Products from './ProductsComponent';
 
 const Main = () => {
     const showCart = useSelector(state => state.cart.showCart);
+    let total = 0;
+    const itemsList = useSelector(state => state.cart.itemsList);
+    itemsList.forEach(element => {
+        total += element.totalPrice;
+    });
         return(
             <div className='bdy'>
                 <Header />
                 <Products />
                 { showCart && <CartItems /> }
+                <div className='container text-right'>
+                    <h3>Total: ${total}</h3>
+                    <Button>Place Order</Button>
+                </div>
             </div>
         );
 };
