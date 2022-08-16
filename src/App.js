1a88014137e6dc108ee0ae51b1575a8a95bf1aaf
Ivan Bowen <bowenivan16@gmail.com>
2022-08-16 21:41:19 +0300

    callback function with an array of dependencies, the dependecy here being cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React  from 'react';
+import React, { useEffect }  from 'react';
 import { useSelector } from 'react-redux';
 import Main from './components/MainComponent';
 import Auth from './components/AuthComponent';
 
 function App() {
   //grab state using useselector hook
+  //let redux manage state then later update the state
+  const cart = useSelector(state => state.cart);
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-  console.log(isLoggedIn);
-  const arr = useSelector((state) => state.cart.itemsList);
-  console.log(arr);
+  // console.log(isLoggedIn);
+  // const arr = useSelector((state) => state.cart.itemsList);
+  // console.log(arr);
+  // will have a callback function with an array of dependencies
+
+  useEffect(() => {
+    fetch('https://redux-shopping-7c9ee-default-rtdb.firebaseio.com/cartitems.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  }, [cart]);
   return(
     <div>
       { !isLoggedIn && <Auth/>}
