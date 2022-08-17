aaf8fe61ceaec0eafa6b97cb151bc54626449361
Ivan Bowen <bowenivan16@gmail.com>
2022-08-17 21:25:05 +0300

    add the reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import AuthSlice from './AuthSlice';
 import CartSlice from './CartSlice';
+import uiSlice from './UiSlice';
 
 // create store then pass in the reducer 
 const store = configureStore({
     reducer: {
         auth: AuthSlice.reducer,
         cart: CartSlice.reducer,
+        ui: uiSlice.reducer
     },
 });
 export default store;
