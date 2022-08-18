319cddc103cbb0e4879c175113bc0e8e851d3c99
Ivan Bowen <bowenivan16@gmail.com>
2022-08-18 17:11:41 +0300

    thunk implementation for sending data to the backend

--- src/store/CartSlice.js
+++ src/store/CartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { uiActions } from "./UiSlice";
 const CartSlice = createSlice({
     name: 'cart',
     initialState: { 
@@ -46,6 +46,41 @@ const CartSlice = createSlice({
         },
     }
 });
+
+export const sendCartData = (cart) => {
+    return async(dispatch) => {
+        dispatch(uiActions.showNotification({
+            open: true,
+            message: 'Sending Request ... ',
+            type: 'warning'
+        }));
+        const sendRequest = async () => {
+            // send state as Sending request
+            
+            const res = await fetch('https://redux-shopping-7c9ee-default-rtdb.firebaseio.com/cartitems.json', {
+              method: 'PUT',
+              body: JSON.stringify(cart),
+            }
+          );
+          const data = await res.json(); // successfull
+          // send state as Request is successful
+          dispatch(uiActions.showNotification({
+            open: true,
+            message: 'Sent Request to Database Successfully',
+            type: 'success'
+          }));
+          };
+          try {
+            await sendRequest();
+          } catch (error) {
+            dispatch(uiActions.showNotification({
+                open: true,
+                message: 'Sending Request Failed',
+                type: 'error'
+              }));
+          }
+    }
+}
 
 export const cartActions = CartSlice.actions;
 export default CartSlice;
